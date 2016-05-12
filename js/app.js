0396596608ad45c674f23bc75f8b5cc3b5c58bd2
Ryca77 <ryan.carter77@gmail.com>
2016-05-12 09:48:49 +0100

    js/app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
+	
 	$(".add").click(function() {
 		var text = $(".stuff").val();
 		if(text.length) {
-			$('ul.note-paper').append('<li>' + text + '<i class="trash fa fa-trash" /> <i class="cart fa fa-cart-plus" /></li>')
+			$('ul.note-paper').append('<li>' + text + '<i class="trash fa fa-trash" /> <i class="cart fa fa-cart-plus" /></li>');
+			$(".stuff").val('');
+		}
+	})
+	$(".stuff").keypress(function(event) {
+		if(event.which == 13) {
+			$(".add").click();
 		}
 	});
+
 });
